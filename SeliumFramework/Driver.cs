e0327f03493f58f9faeaa7429bf411fd84362e4d
Evgeni-h <99342237+Evgeni-h@users.noreply.github.com>
2022-02-12 18:18:33 +0200

    Add files via upload

---
+++ SeliumFramework/Driver.cs
@@ -0,0 +1,61 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeleniumFramework
+{
+    public class Driver
+    {
+        public static IWebDriver Instance { get; set; }
+        public static void Initialize()
+        {
+            Instance = new ChromeDriver();
+            Instance.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(7);
+            Instance.Manage().Window.Maximize();
+        }
+
+        internal static void SwitchToFrame(Helper.SelectorType selectorType, string selectorString)
+        {
+            switch (selectorType)
+            {
+                case Helper.SelectorType.Id:
+                    break;
+                case Helper.SelectorType.Name:
+                    break;
+                case Helper.SelectorType.CssSelector:
+                    break;
+                case Helper.SelectorType.ClassName:
+                    break;
+                case Helper.SelectorType.LinkText:
+                    break;
+                case Helper.SelectorType.XPath:
+                    Driver.Instance.SwitchTo().Frame(Driver.Instance.FindElement(By.XPath(selectorString)));
+                    break;
+                case Helper.SelectorType.TagName:
+                    break;
+                case Helper.SelectorType.PartialLinkText:
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        internal static void SwitchToDefaultFrame()
+        {
+            Driver.Instance.SwitchTo().DefaultContent();
+        }
+
+        public static void GoBack()
+        {
+            Driver.Instance.Navigate().Back();
+        }
+        public static void Close()
+        {
+            Instance.Quit();
+        }
+    }
+}
---
+++ SeliumFramework/MainMenu.cs
@@ -0,0 +1,21 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeleniumFramework
+{
+    public class MainMenu
+    {
+        public class MediaLibraryTab
+        {
+            public static void Select()
+            {
+                var mediaLibraryTab = Driver.Instance.FindElement(By.CssSelector(".ModuleNavigationLinkstyled__StyledNavLink-sc-1cquk99-1.gsSnVV.module-link.module-link.media"));
+                mediaLibraryTab.Click();
+            }
+        }
+    }
+}
